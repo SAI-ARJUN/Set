f2839bf8ccf65e7dab4cceb7be3df21c88801212
SAI ARJUN <112474122+SAI-ARJUN@users.noreply.github.com>
2022-09-02 19:38:10 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,107 @@
+#Unindexed
+#set={1,2,3}
+#print(set)
+#print(set[0])  #Object is not subsricptable
+
+
+#Unordered
+#set1={4,5,6}
+#set2={1,2,3}
+#print(set1==set2)    #False
+
+#Not allowing duplication
+#set={1,2,3,1,2,3,3,3}
+#print(set)             #{1,2,3}
+
+
+#Adding element to the set
+
+#myset=set(['a','b'])
+#print(myset)
+#myset.add('c')
+#print(myset)               #{a,b,c}
+
+
+#Union
+#a={1,2,3}
+#b={4,5,6}
+#x=a.union(b)
+#print(x)
+#y=a|b
+#print(y)           #{1,2,3,4,5,6}
+
+
+
+
+#Intersection
+#a={1,2,3}
+#b={3,5,6}
+#x=a.intersection(b)
+#print(x)
+#y=a&b
+#print(y)           #{3}
+
+
+#Difference
+#a={1,2,3}
+#b={3,5,6}
+#x=a.difference(b)
+#print(x)            #{1,2}
+#y=a-b
+#print(y)
+
+
+#Symmetric Difference
+#a={1,2,3}
+#b={3,5,6}           
+#y=a^b          #{1,2,5,6}
+#print(y)   
+
+
+#Clearing Set
+#set1 = {1,2,3,4,5,6} 
+#print("Initial set")
+#print(set1)
+#set1.clear() 
+#print("\nSet after using clear() function")
+#print(set1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
